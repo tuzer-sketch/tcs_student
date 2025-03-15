ccde34b0773e22ad9884e3211a87cc41ee65f60d
noob999-hacker <natalie027784@gmail.com>
2025-03-15 09:56:08 -0700

    3/15
    We added more rooms an doors

---
+++ Natalie Leung/A Decision Game Horror (Don't have a real name yet)/Assets/Scripts/Room handler.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Roomhandler : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+        
+    }
+
+    void Awake()
+    {
+        DontDestroyOnLoad (this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
